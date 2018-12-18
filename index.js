160ada553a5ca1ea7f2b08f7c55fbdcc5d301fd7
Jeff Hykin <jeffhykin@Jeffs-MacBook-Pro-2.local>
2018-12-18 12:53:20 -0600

    remove the head setter (good-jsx now handles it)

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 module.exports = {
     generateFrontend: () => `
         require("good-jsx")
-        let originalHead = document.head
-        // add a setter to document.head
-        Object.defineProperty(document,"head", { 
-            set: (element) => {
-                document.head.add(...element.childNodes)
-            },
-            get: ()=>originalHead
-        })
     `
 }
